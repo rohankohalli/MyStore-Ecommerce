d4b3d1e0aedbf464ad2d5ef0e68e507b33a2bc0d
rohankohalli <rohan.kohalli@gmail.com>
2026-03-11 15:05:36 +0530

    changed login rate limit

--- backend/src/utils/loginLimit.js
+++ backend/src/utils/loginLimit.js
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit"
 
 export const loginLimit = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 10,
+    windowMs: 10 * 60 * 1000,
+    max: 5,
     message: "Too many login attempts"
 })
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
         <CartProvider>
-            <App />
+            <StrictMode>
+                <App />
+            </StrictMode>
         </CartProvider>
     </AuthProvider>
 )
