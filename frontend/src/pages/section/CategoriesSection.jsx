7f93c519c1dcf018566bf9e34c4434fd389ce1f0
rohankohalli <rohan.kohalli@gmail.com>
2026-03-17 17:06:53 +0530

    feat(categories): add horizontal scroll navigation and hide scrollbar

--- frontend/src/pages/section/CategoriesSection.jsx
+++ frontend/src/pages/section/CategoriesSection.jsx
@@ -1,33 +1,51 @@
 import { Link } from "react-router-dom"
 import { Categories } from "../../constants/categories"
-import { useState } from "react"
+import { useRef } from "react"
+import { ArrowLeft, ArrowRight } from "lucide-react"
 
 const CategoriesSection = () => {
-    const [showAll, setShowAll] = useState(false)
 
-    const showCtgs = showAll ? Categories : Categories.slice(0, 5)
+    const scrollRef = useRef(null)
+
+    const scroll = (direction) => {
+        const amount = 250
+
+        if (direction === "left") {
+            scrollRef.current.scrollLeft -= amount
+        } else {
+            scrollRef.current.scrollLeft += amount
+        }
+    }
 
     return (
-        <div>
-            <div className="flex justify-between items-center mt-2">
-                <h4>Shop By Category</h4>
+        <div className="mt-2">
+            <h4>Shop By Category</h4>
+            <div className="relative">
 
-                <button
-                    onClick={() => setShowAll(prev => !prev)}
-                    className="text-blue-600 cursor-pointer">
-                    {showAll ? 'Show less' : 'View all'}
+                <button onClick={() => scroll("left")}
+                    className="cursor-pointer absolute left-0 top-1/2 -translate-y-1/2 bg-white border rounded-lg p-1 shadow">
+                    <ArrowLeft />
                 </button>
-            </div>
-            <div className="flex gap-4 border rounded-lg p-2 flex-wrap">
-                {showCtgs.map(ctg => (
-                    <Link to={`/list?category=${ctg.value}`} key={ctg.value}
-                        className="flex flex-col items-center gap-4 border rounded p-4 w-3xs shrink-0 hover:shadow-lg">
-                        <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center">
-                            <span className="text-sm">{ctg.label[0]}</span>
-                        </div>
-                        <span className="text-sm">{ctg.label}</span>
-                    </Link>
-                ))}
+                <div
+                    ref={scrollRef}
+                    className="flex gap-4 border rounded-lg p-2 overflow-x-auto flex-nowrap scroll-smooth no-scrollbar">
+
+                    {Categories.map(ctg => (
+                        <Link to={`/list?category=${ctg.value}`} key={ctg.value}
+                            className="flex flex-col items-center gap-4 border rounded p-4 w-3xs shrink-0 hover:shadow-lg">
+                            <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center">
+                                <span className="text-sm">{ctg.label[0]}</span>
+                            </div>
+                            <span className="text-sm">{ctg.label}</span>
+                        </Link>
+
+
+                    ))}
+                </div>
+                <button onClick={() => scroll("right")}
+                    className="cursor-pointer absolute right-0 top-1/2 -translate-y-1/2 bg-white border rounded-lg p-1 shadow">
+                    <ArrowRight />
+                </button>
             </div>
         </div>
     )
